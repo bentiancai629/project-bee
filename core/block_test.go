f7d611cccb75e4311da1862ccb56419381f9ab98
nick <bentiancai629@gmail.com>
2024-04-30 12:21:34 +0800

    ep2

---
+++ core/block_test.go
@@ -0,0 +1,66 @@
+package core
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+	"time"
+
+	"project-bee/types"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Header_Encode_Decode(t *testing.T) {
+	h := &Header{
+		Version:   1,
+		PrevBlock: types.RandomHash(),
+		Timestamp: time.Now().UnixNano(),
+		Height:    10,
+		Nonce:     989394,
+	}
+	buf := &bytes.Buffer{}
+	assert.Nil(t, h.EncodeBinary(buf))
+
+	hDecode := &Header{}
+	assert.Nil(t, hDecode.DecodeBinary(buf))
+	assert.Equal(t, h, hDecode)
+}
+
+func Test_Block_Encode_Decode(t *testing.T) {
+	b := &Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: time.Now().UnixNano(),
+			Height:    100,
+			Nonce:     1,
+		},
+		Transactions: nil,
+	}
+
+	buf := &bytes.Buffer{}
+	assert.Nil(t, b.EncodeBinary(buf))
+
+	bDecode := &Block{}
+	assert.Nil(t, bDecode.DecodeBinary(buf))
+	assert.Equal(t, b, bDecode)
+}
+
+func TestBlockHash(t *testing.T) {
+	b := &Block{
+		Header: Header{
+			Version:   1,
+			PrevBlock: types.RandomHash(),
+			Timestamp: time.Now().UnixNano(),
+			Height:    100,
+			Nonce:     1,
+		},
+		Transactions: []Transaction{},
+	}
+
+	h := b.Hash()
+
+	fmt.Println("hash:", h)
+	assert.False(t, h.IsZero())
+}
